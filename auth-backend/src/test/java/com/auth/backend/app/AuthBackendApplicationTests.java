018227c22df5806ddb2c02739d2400cde8e9f79a
Saswat Pal <saswat2001@gmail.com>
2026-04-11 18:20:00 +0530

    Initial commit

---
+++ auth-backend/src/test/java/com/auth/backend/app/AuthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.backend.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
